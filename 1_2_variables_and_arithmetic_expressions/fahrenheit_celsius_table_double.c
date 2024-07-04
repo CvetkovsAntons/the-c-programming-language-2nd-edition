0a3c4a4b79e443fe14355e52e09d879b0c3efcf1
Antons Cvetkovs <antonscvetkovs12@gmail.com>
2024-07-04 07:44:19 +0300

    1.2 chapter solutions with float and double, added .gitignore file

--- 1_2_variables_and_arithmetic_expressions/fahrenheit_celsius_table_double.c
+++ 1_2_variables_and_arithmetic_expressions/fahrenheit_celsius_table_double.c
@@ -2,15 +2,15 @@
 
 // this program prints out Fahrenheit-Celsius table from Fahrenheit equals 0 to 300 with step of 20
 int main() {
-    int fahrenheit = 0, celsius;
+    double fahrenheit = 0, celsius;
     int max = 300, step = 20;
 
     printf("%6s |%6s\n", "F", "C");
     printf("-------|-------\n");
 
     while (fahrenheit <= max) {
-        celsius = 5 * (fahrenheit - 32) / 9;
-        printf("%6d |%6d\n", fahrenheit, celsius);
+        celsius = (5.0 / 9.0) * (fahrenheit - 32);
+        printf("%6.2lf |%6.2lf\n", fahrenheit, celsius);
         fahrenheit += step;
     }
 }
--- 1_2_variables_and_arithmetic_expressions/fahrenheit_celsius_table_float.c
+++ 1_2_variables_and_arithmetic_expressions/fahrenheit_celsius_table_float.c
@@ -2,15 +2,15 @@
 
 // this program prints out Fahrenheit-Celsius table from Fahrenheit equals 0 to 300 with step of 20
 int main() {
-    int fahrenheit = 0, celsius;
+    float fahrenheit = 0, celsius;
     int max = 300, step = 20;
 
     printf("%6s |%6s\n", "F", "C");
     printf("-------|-------\n");
 
-    while (fahrenheit <= max) {
-        celsius = 5 * (fahrenheit - 32) / 9;
-        printf("%6d |%6d\n", fahrenheit, celsius);
-        fahrenheit += step;
+    while (fahrenheit <= (float)max) {
+        celsius = (5.0f / 9.0f) * (fahrenheit - 32.0f);
+        printf("%6.2f |%6.2f\n", fahrenheit, celsius);
+        fahrenheit += (float)step;
     }
 }
