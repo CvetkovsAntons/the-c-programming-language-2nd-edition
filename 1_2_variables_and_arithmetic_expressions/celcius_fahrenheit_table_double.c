76727aaad614a85d7639e3be4eb78dd08fcf91e9
Antons Cvetkovs <antonscvetkovs12@gmail.com>
2024-07-04 07:53:30 +0300

    1.2 chapter, second task solution

---
+++ 1_2_variables_and_arithmetic_expressions/celcius_fahrenheit_table_double.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// this program prints out Fahrenheit-Celsius table from Fahrenheit equals 0 to 300 with step of 20
+int main() {
+    double celsius = 0, fahrenheit;
+    int max = 300, step = 20;
+
+    printf("%6s |%6s\n", "C", "F");
+    printf("-------|-------\n");
+
+    while (celsius <= max) {
+        fahrenheit = (9.0 / 5.0 * celsius) + 32;
+        printf("%6.2lf |%6.2lf\n", celsius, fahrenheit);
+        celsius += step;
+    }
+}
